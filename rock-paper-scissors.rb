658e7209d0be9163226e71b592d4b35aa83cbf8c
emily.c.ritter <emily.c.ritter@gmail.com>
2016-02-16 14:23:01 -0600

    Add difficult mode (within same class).

--- rock-paper-scissors.rb
+++ rock-paper-scissors.rb
@@ -1,31 +1,44 @@
 class Game
-  attr_accessor :player_one_choice, :computer, :player_one_name, :computer_name
+  attr_accessor :player_one_choice, :computer, :player_one_name, :computer_name, :winner, :winning_choice
 
   def initialize
     self.player_one_name = "Player"
     self.computer_name = "Computer"
+    self.winner = ""
+    self.winning_choice = ""
   end
 
   def play
-    get_user_choice
-    get_computer_choice
-    determine_winner
-  end
-
-  def play_prompt
-    self.player_one_choice = ""
-    answer = ""
     puts "=============================================="
     puts ""
+    answer = ""
     until (["play", "p", "quit", "q", "run", "r"].include? answer) do
-      puts "Play, quit, or run simulation. ('play' / 'quit' / 'run')"
+      puts "Play, run simulation, or quit. ('play' / 'run' / 'quit')"
       answer = gets.chomp.downcase
     end
+
     if (answer == "play" || answer == "p")
-      play
+      play_user
     elsif (answer == "run" || answer == "r")
       run_simulation
     end
+
+  end
+
+  def play_user
+    self.player_one_choice = ""
+    self.player_one_name = "Player"
+    get_user_choice
+    get_computer_choice
+    determine_winner
+
+    output_msg = "#{self.player_one_name}: #{self.player_one_choice.upcase} vs #{self.computer_name}: #{self.computer.upcase} -----"
+    if self.winner == "TIE"
+      puts "#{output_msg} TIE"
+    else
+      puts "#{output_msg} #{winner} WINS"
+    end
+    play
   end
 
   def get_user_choice
@@ -37,62 +50,72 @@ class Game
 
   def get_computer_choice
     self.computer = ["rock", "paper", "scissors"].sample
-    # puts self.computer
   end
 
   def determine_winner
-    output_msg = "#{self.player_one_name}: #{self.player_one_choice.upcase} vs #{self.computer_name}: #{self.computer.upcase} -----"
-    winner = self.player_one_name.upcase
 
     if self.player_one_choice == self.computer
-      puts "#{output_msg} TIE"
-      winner = "TIE"
+      self.winner = "TIE"
     else
       if self.player_one_choice == "rock"
-        if self.computer != "scissors"
-          winner = self.computer_name.upcase
+        if self.computer == "scissors"
+          self.winner = self.player_one_name.upcase
+        else
+          self.winner = self.computer_name.upcase
         end
       elsif self.player_one_choice == "paper"
-        if self.computer != "rock"
-          winner = self.computer_name.upcase
+        if self.computer == "rock"
+          self.winner = self.player_one_name.upcase
+        else
+          self.winner = self.computer_name.upcase
         end
       else # player = scissors
-        if self.computer != "paper"
-          winner = self.computer_name.upcase
+        if self.computer == "paper"
+          self.winner = self.player_one_name.upcase
+        else
+          self.winner = self.computer_name.upcase
         end
       end
-      puts "#{output_msg} #{winner} WINS"
     end
-    return winner
+
+    if self.winner == self.player_one_name.upcase
+      self.winning_choice = self.player_one_choice
+    elsif self.winner == self.computer_name.upcase
+      self.winning_choice = self.computer
+    else
+      self.winning_choice = "TIE"
+    end
   end
 
   def run_simulation
     self.player_one_name = "Player (auto)"
     tie_count = 0
-    player_count = 0
-    computer_count = 0
+    rock_count = 0
+    paper_count = 0
+    scissors_count = 0
 
     (1..1000).each do
       self.player_one_choice = get_computer_choice
       get_computer_choice
-      play
-      if play == "TIE"
-        tie_count += 1
-      elsif play == self.player_one_name.upcase
-        player_count += 1
-      else
-        computer_count += 1
+      determine_winner
+      case self.winning_choice
+      when "TIE"; tie_count += 1
+      when "rock"; rock_count += 1
+      when "paper"; paper_count += 1
+      when "scissors"; scissors_count += 1
       end
     end
 
     puts "Ties: #{tie_count} (#{100*(tie_count/1000.00).round(2)}%)"
-    puts "#{self.player_one_name}: #{player_count} (#{100*(player_count/1000.00).round(2)}%)"
-    puts "#{self.computer_name}: #{computer_count} (#{100*(computer_count/1000.00).round(2)}%)"
-    play_prompt
+    puts "Rock: #{rock_count} (#{100*(rock_count/1000.00).round(2)}%)"
+    puts "Paper: #{paper_count} (#{100*(paper_count/1000.00).round(2)}%)"
+    puts "Scissors: #{scissors_count} (#{100*(scissors_count/1000.00).round(2)}%)"
+
+    play
 
   end
 
 end
 
 game = Game.new
-game.play_prompt
+game.play
